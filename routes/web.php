2d7d38f10bbfa630c6290f858831da0e6fec7583
Bundy <github@andrashorvath.dev>
2026-02-13 17:33:45 +0100

    cf

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('pages.home'))->name('home');
+Route::get('/', function () {
+    return response()
+        ->view('pages.home')
+        ->header('Cache-Control', 'public, s-maxage=3600, max-age=0');
+})->name('home');
 
 Route::get('/contact-fragment', function () {
     return view('partials.contact-form-fragment');
