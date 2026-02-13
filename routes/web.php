458f79e64d6a6f23864d9a7251770e9dbe63a9c3
Bundy <github@andrashorvath.dev>
2026-02-13 17:44:28 +0100

    cf

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+
+
 
 Route::get('/', function () {
     return response()
         ->view('pages.home')
         ->header('Cache-Control', 'public, s-maxage=3600, max-age=0');
-})->name('home');
+})
+->name('home')
+->withoutMiddleware([
+    StartSession::class,
+    ShareErrorsFromSession::class,
+    VerifyCsrfToken::class,
+    EncryptCookies::class,
+    AddQueuedCookiesToResponse::class,
+]);
 
 Route::get('/contact-fragment', function () {
     return view('partials.contact-form-fragment');
