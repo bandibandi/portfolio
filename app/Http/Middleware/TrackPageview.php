653c2763bb033d5c079bf3d5b3df8a7dbffc6857
Bundy <github@andrashorvath.dev>
2026-03-11 14:25:46 +0100

    pirsch

--- app/Http/Middleware/TrackPageview.php
+++ app/Http/Middleware/TrackPageview.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Pirsch\Http\Middleware\TrackPageview as Middleware;
 
 class TrackPageview extends Middleware
@@ -9,4 +12,15 @@ class TrackPageview extends Middleware
     protected array $except = [
         'admin/*',
     ];
+
+    public function handle(Request $request, Closure $next, string ...$excepts): mixed
+    {
+        Log::info('Pirsch middleware fired', [
+            'url' => $request->fullUrl(),
+            'ip' => $request->ip(),
+            'token_set' => ! empty(config('pirsch.token')),
+        ]);
+
+        return parent::handle($request, $next, ...$excepts);
+    }
 }
