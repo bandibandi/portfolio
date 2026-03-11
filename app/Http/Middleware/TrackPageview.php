590cb0630ee17c2986e9e7ef05a2b5ec9c5f63c7
Bundy <github@andrashorvath.dev>
2026-03-11 14:32:46 +0100

    terminate

--- app/Http/Middleware/TrackPageview.php
+++ app/Http/Middleware/TrackPageview.php
@@ -15,11 +15,17 @@ class TrackPageview extends Middleware
 
     public function handle(Request $request, Closure $next, string ...$excepts): mixed
     {
-        Log::info('Pirsch middleware fired', [
+        Log::info('Pirsch middleware handle()', [
             'url' => $request->fullUrl(),
             'ip' => $request->ip(),
             'token_set' => ! empty(config('pirsch.token')),
         ]);
+
+        app()->terminating(function () use ($request) {
+            Log::info('Pirsch terminating callback fired', [
+                'url' => $request->fullUrl(),
+            ]);
+        });
 
         return parent::handle($request, $next, ...$excepts);
     }
